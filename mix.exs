a9f669e68b66390efd7a6ca2b8f8a65cb91600e4
Jesper Dalberg <JDNLN@nuuday.dk>
2022-03-28 17:56:23 +0200

    new elixir, new style

--- mix.exs
+++ mix.exs
@@ -3,21 +3,21 @@ defmodule MyAcs.Mixfile do
 
   def project do
     [app: :my_acs,
-     version: "0.1.2",
-     elixir: "~> 1.8",
+     version: "0.1.3",
+     elixir: "~> 1.13",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
   def application do
-    [applications: [:logger, :acs_ex],
+    [
      mod: {MyAcs, []}]
   end
 
   defp deps do
     [
-      {:acs_ex, "~> 0.3.3"}
+      {:acs_ex, "~> 0.3.18"}
     ]
   end
 end
