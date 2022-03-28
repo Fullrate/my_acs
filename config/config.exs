c8a5b6c12803c0a5ca21e91ab7295097946d13b6
Jesper Dalberg <JDNLN@nuuday.dk>
2022-03-28 17:55:57 +0200

    New style config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
-use Mix.Config
+import Config
 
-config :acs_ex, :acs_port, 7547
+config :acs_ex, :acs_port, 7548
 config :acs_ex,
   crypt_keybase: "31de9f7d766287c7565801f30babbd4f",
   crypt_cookie_salt: "SomeSalt",
